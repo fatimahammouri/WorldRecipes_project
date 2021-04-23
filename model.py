2cab0f7ccfe3fa26e4792c3372aba76e0405ce54
Fatima <fatima.hammouri@gmail.com>
2021-04-23 22:10:52 +0000

    workin on Models

--- model.py
+++ model.py
@@ -5,6 +5,42 @@ db = SQLAlchemy()
 
 
 
+class User(db.Model):
+    """Users Table"""
+
+    __tablename__ = "users"
+
+    user_id = db.Column(db.Integer, primary_key=True,
+                        autoincrement=True)
+    email = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(50))
+    
+    def __repr__(self):
+        return f"<<User user_id={self.user_id} email={self.email}>>"
+####################################################################################
+
+class Recipe(db.Model):
+    """Recipes Table"""
+
+    __tablename__ = "recipes"
+
+    recipe_id = db.Column(db.Integer, primary_key=True,
+                        autoincrement=True)
+    recipe_name = db.Column(db.String(100))
+    description = db.Column(db.Text)
+    photo = db.Column(db.String(100))
+    cuisine_id = db.Column(db.Integer, db.ForeignKey("cuisines.cuisine_id"))
+    ingredient_id = db.column(db.Integer, db.ForeignKey("ingredients.ingredient_id"))
+
+
+    def __repr__(self):
+        return f'<<Recipe recipe_id={self.recipe_id} name={self.recipe_name}>>'
+####################################################################################
+
+
+
+
+
 def connect_to_db(flask_app, db_uri='postgresql:///recipes', echo=True):
     flask_app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
     flask_app.config['SQLALCHEMY_ECHO'] = echo
--- server.py
+++ server.py
@@ -1,8 +1,13 @@
-from flask import Flask, render_template
+from flask import (Flask, render_template, request, flash, session,
+                   redirect)
 
 app = Flask(__name__) 
+app.secret_key = "recipe"
 
 
+@app.route("/")
+def homepage():
+    return render_template("homepage.html")
 
 
 app.run(host='0.0.0.0', debug=True, port=5000)
