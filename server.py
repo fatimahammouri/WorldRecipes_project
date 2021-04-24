3a2a7273f090f371103f2243c0caf6c816313844
Fatima <fatima.hammouri@gmail.com>
2021-04-24 01:06:25 +0000

    setting up to use API

--- server.py
+++ server.py
@@ -1,12 +1,18 @@
 from flask import (Flask, render_template, request, flash, session,
                    redirect)
+from pprint import pformat, pprint
+import os
+import requests
 
 from model import connect_to_db
 
-app = Flask(__name__) 
+
+app = Flask(__name__)
 app.secret_key = "recipe"
 
+API_KEY = os.environ['SPOON_KEY'] 
 
+########################################################################
 @app.route("/")
 def homepage():
     return render_template("homepage.html")
