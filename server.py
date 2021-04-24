c72f85276c93e4503d2b0dfe6761671fa1345c92
Fatima <fatima.hammouri@gmail.com>
2021-04-24 03:30:19 +0000

    simple user login on the homepage

--- server.py
+++ server.py
@@ -10,12 +10,17 @@ from model import connect_to_db
 app = Flask(__name__)
 app.secret_key = "recipe"
 
-API_KEY = os.environ['SPOON_KEY'] 
+# API_KEY = os.environ['SPOON_KEY'] 
 
 ########################################################################
 @app.route("/")
 def homepage():
     return render_template("homepage.html")
+
+@app.route("/login", methods=['POST'])
+def handle_login():
+    username = request.form['username']
+    password = request.form['password']
 
 # when creating end points, i will use
 # any paremeters needed Ex:keyword = request.args.get('keyword', '')
