0299da9db7d1d98b81a03dd79c724796d4911608
Fatima <fatima.hammouri@gmail.com>
2021-04-24 03:51:00 +0000

    created basic login/register pages

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ import os
 import requests
 
 from model import connect_to_db
-
+import crud
 
 app = Flask(__name__)
 app.secret_key = "recipe"
@@ -17,10 +17,23 @@ app.secret_key = "recipe"
 def homepage():
     return render_template("homepage.html")
 
-@app.route("/login", methods=['POST'])
-def handle_login():
-    username = request.form['username']
+# @app.route("/login", methods=['POST'])
+# def handle_login():
+#     username = request.form['username']
+#     password = request.form['password']
+
+@app.route('/register', methods=['POST'])
+def register_user():
+    email = request.form['email']
     password = request.form['password']
+
+    user = get_user_by_email(email)
+    if user:
+        return 'A user already exists with that email.'
+    else:
+        create_user(email, password)
+
+        return redirect('/')
 
 # when creating end points, i will use
 # any paremeters needed Ex:keyword = request.args.get('keyword', '')
