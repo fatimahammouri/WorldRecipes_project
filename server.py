ebe29034eb5a0b6d99c42b5e617247ec2dcf9c33
Fatima <fatima.hammouri@gmail.com>
2021-05-02 03:54:57 +0000

    apply parse_recipe_details func to our complex results

--- server.py
+++ server.py
@@ -87,9 +87,8 @@ def show_results():
     response = requests.get(url, params)
     data = response.json()
 
-    title = data['recipes'][0]['title']
-    instructions = data['recipes'][0]['instructions']
-    image = data['recipes'][0]['image']
+    results = data['results']
+    recipe_results = parse_api.parse_recipe_details(results)
 
     return render_template("results.html", title=title, image=image,
                             instructions=instructions)
