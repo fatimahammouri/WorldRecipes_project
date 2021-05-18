78f03235468244494907f2f8224809eeaf3f939a
Fatima <fatima.hammouri@gmail.com>
2021-05-18 12:44:23 +0000

    made recipeData part of state so it  appears when the componant re-render

--- static/js/Recipes.jsx
+++ static/js/Recipes.jsx
@@ -16,6 +16,7 @@ function Recipe(props){
 }
 
 function Recipes(props){
+    const [recipeData, setRecipeData] = React.useState(null);
     return(
         <div>
             Recipes from API will be here
