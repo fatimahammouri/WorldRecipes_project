d9f7a75acf3616cca10827b7d0bfc1e8b73417e3
Fatima <fatima.hammouri@gmail.com>
2021-05-18 12:34:27 +0000

    created a Recipe Component

---
+++ static/js/Recipes.jsx
@@ -0,0 +1,17 @@
+"use strict";
+
+function Recipe(props){
+    const { title, servings, readyInMinutes,
+            instructions, image, ingredients, sourceUrl }
+    return(
+        <div>
+            <h2> {title} </h2>
+            <p> {servings} - {readyInMinutes} </p>
+            <p> {instructions} - {ingredients} </p>
+            <a href={sourceUrl}>recipe source</a>
+            <img src={image} />
+        
+        </div> 
+
+    )
+}
